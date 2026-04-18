768d4fc83a458d5ed172768480cab6094b1c5aab
Thom Manning <84347286+manni155@users.noreply.github.com>
2026-04-18 08:18:50 +0000

    completed

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add JavaScript below
+async function fetchPost() {
+  
+  try {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts/1`)
+
+    if (!response.ok){
+      throw new Error("could not fetch resource")
+    }
+
+    const data = await response.json();
+    console.log(data);
+
+    const postid = data['id'];
+    const postTitle = data['title']
+    const postBody = data['body']
+    const postUser = data['userId']
+
+    document.getElementById('postId').innerText = postid
+    document.getElementById('title').innerText = postTitle
+    document.getElementById('body').innerText = postBody
+    document.getElementById('userId').innerText = postUser
+  }
+  catch(error) {
+    console.error(error);
+  }
+}
